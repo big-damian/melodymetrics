d280507bf7800f0ee91c5f25f96a1d6433d3af78
big-damian <bdpmxd2@gmail.com>
2024-12-25 19:49:17 +0100

    Finished method to download dataset from kaggle and bumped code version

--- melodymetrics/dataset/kaggledownload.py
+++ melodymetrics/dataset/kaggledownload.py
@@ -2,7 +2,8 @@ import kagglehub
 import shutil
 import os
 
-class kaggledownload:
+class KaggleDownload:
+
     def __init__(self):
         """
         Initialize the KaggleDownload class.
@@ -11,15 +12,21 @@ class kaggledownload:
         - download_path (str): Datasets that will be downloaded.
         """
 
-    def dataset_download(self, dataset_id="paradisejoy/top-hits-spotify-from-20002019", file_path="songs_normalize.csv"):
+    @staticmethod
+    def dataset_download(dataset_id="paradisejoy/top-hits-spotify-from-20002019",
+                         file_path="songs_normalize.csv",
+                         new_dataset_filename="spotify_top_hits_2020.csv"):
         """
         Download and extract a Kaggle dataset.
 
         Parameters:
         - dataset_id (str): The Kaggle dataset identifier (e.g., "paradisejoy/top-hits-spotify-from-20002019").
         - path (str): The Kaggle path to single file (e.g., "songs_normalize.csv").
+        - new_dataset_filename (str): New filename for the dataset (e.g., "spotify_top_hits_2020.csv")
         """
+
         print(f"Downloading dataset: {dataset_id}")
+
         try:
             # Download the dataset
             dataset_download_path = kagglehub.dataset_download('paradisejoy/top-hits-spotify-from-20002019', path='songs_normalize.csv', )
@@ -27,16 +34,11 @@ class kaggledownload:
 
             # Move the dataset file
             shutil.move(dataset_download_path, os.getcwd())
-            print(f"Dataset downloaded to: {os.getcwd()}")
+
+            # Rename the dataset file
+            os.rename(file_path, new_dataset_filename)
+
+            print(f"Dataset downloaded to: {os.getcwd()}\\{new_dataset_filename}")
 
         except Exception as e:
             print(f"Error downloading dataset: {e}")
-
-    def rename_dataset(self, new_name="spotify_top_hits_2020"):
-        """
-        Download and extract a Kaggle dataset.
-
-        Parameters:
-        - new_name (str): New name for the dataset file (e.g., "spotify_top_hits_2020").
-        """
-        # TODO: Finish this method
