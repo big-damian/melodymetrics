91b0be326102f3369c58f49c508577c7c3ef5c57
big-damian <bdpmxd2@gmail.com>
2025-01-19 04:30:32 +0100

    Added docstrings and comments - main.py

--- melodymetrics/main.py
+++ melodymetrics/main.py
@@ -1,11 +1,21 @@
 """
-Main module. Runs the app starting with its main window
+Main module to launch the MelodyMetrics application.
+
+This module serves as the entry point for the application, initializing
+and running the main graphical user interface (GUI).
 """
 
 from melodymetrics.gui.main_window import MainWindow
 
 
 def main():
+    """
+    Initializes and runs the main window of the MelodyMetrics application.
+
+    This function creates an instance of the MainWindow class and calls
+    its `run` method to start the application's GUI.
+    """
+
     # Create and run the main window for the app
     app = MainWindow()
     app.run()
