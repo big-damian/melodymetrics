bd5f933c70f82e4d1136677dc056236d86e539a9
big-damian <bdpmxd2@gmail.com>
2025-01-19 07:58:37 +0100

    Added custom exception DatasetFileNotFoundException

--- melodymetrics/exceptions.py
+++ melodymetrics/exceptions.py
@@ -23,10 +23,46 @@ class DatasetNotLoadedException(Exception):
         ----------
         message : str, optional
             The error message to display. Defaults to "No dataset loaded yet.
-            Please load the dataset first" if "default" is passed.
+            Please load the dataset first." if "default" is passed.
         """
         if message == "default":
-            message = "No dataset loaded yet. Please load the dataset first"
+            message = "No dataset loaded yet. Please load the dataset first."
+        super().__init__(message)
+
+    def __str__(self):
+        """
+        Returns the string representation of the exception.
+
+        Returns
+        -------
+        str
+            The error message.
+        """
+        print(f"{self.args[0]}")  # Print the error message
+        return f"{self.args[0]}"
+
+class DatasetFileNotFoundException(Exception):
+    """
+    Exception raised when a dataset file is not found.
+
+    Attributes
+    ----------
+    message : str
+        A descriptive message indicating the cause of the error.
+    """
+
+    def __init__(self, message="default"):
+        """
+        Initializes the DatasetFileNotFoundException with a message.
+
+        Parameters
+        ----------
+        message : str, optional
+            The error message to display. Defaults to "No dataset file found in directories.
+            Please download the dataset first." if "default" is passed.
+        """
+        if message == "default":
+            message = "No dataset file found in directories. Please download the dataset first."
         super().__init__(message)
 
     def __str__(self):
