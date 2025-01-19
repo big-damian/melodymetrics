dea889dfab2b0115f0e0fa6e82cda580f7bc38e2
big-damian <bdpmxd2@gmail.com>
2025-01-19 04:33:06 +0100

    Added docstrings and comments - exceptions.py

--- melodymetrics/exceptions.py
+++ melodymetrics/exceptions.py
@@ -1,16 +1,42 @@
+"""
+Module for handling custom exceptions in the application.
+
+This module defines custom exceptions used in the application.
+"""
+
+
 class DatasetNotLoadedException(Exception):
     """
     Exception raised when a dataset is not loaded.
 
-    Attributes:
-        message (str): A descriptive message indicating the cause of the error.
+    Attributes
+    ----------
+    message : str
+        A descriptive message indicating the cause of the error.
     """
 
     def __init__(self, message="default"):
+        """
+        Initializes the DatasetNotLoadedException with a message.
+
+        Parameters
+        ----------
+        message : str, optional
+            The error message to display. Defaults to "No dataset loaded yet.
+            Please load the dataset first" if "default" is passed.
+        """
         if message == "default":
             message = "No dataset loaded yet. Please load the dataset first"
         super().__init__(message)
 
     def __str__(self):
-        print(f"{self.args[0]}")
+        """
+        Returns the string representation of the exception.
+
+        Returns
+        -------
+        str
+            The error message.
+        """
+        print(f"{self.args[0]}")  # Print the error message
         return f"{self.args[0]}"
